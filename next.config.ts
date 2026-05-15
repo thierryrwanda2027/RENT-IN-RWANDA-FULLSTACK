2f58dcaeb6b16409217bc1a5a7ce3cb7dc4c73a4
thierryrwanda2027 <theniyonkuru@gmail.com>
2026-05-15 14:12:22 +0200

    update package configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 import withSerwistInit from "@serwist/next";
-import path from "path";
 
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
@@ -11,23 +10,19 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.unsplash.com',
+        protocol: "https",
+        hostname: "**.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'www.houseinrwanda.com',
+        protocol: "https",
+        hostname: "www.houseinrwanda.com",
       },
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
       },
     ],
-  },
-  turbopack: {
-    root: path.resolve(process.cwd()),
   },
 };
 
 export default withSerwist(nextConfig);
-
